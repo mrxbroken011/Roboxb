1d981ff1567eed5c8cba6704a8905de21f91943a
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-06-10 21:20:47 +0530

    Update chatgpt.py

--- JarvisRobo/modules/chatgpt.py
+++ JarvisRobo/modules/chatgpt.py
@@ -12,7 +12,7 @@ async def chat_gpt(bot, message):
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
         if len(message.command) < 2:
             await message.reply_text(
-            "Example:**\n\n`/chatgpt Where is TajMahal?`")
+            "ʜᴇʟʟᴏ sɪʀ/ᴍᴀᴅᴀᴍ\nExample:**\n\n` Broken Where is TajMahal?`")
         else:
             a = message.text.split(' ', 1)[1]
             r=api.gemini(a)["results"]

